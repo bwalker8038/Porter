c7c053cf86f4d552ac8883640284abe758351f9e
Brad Walker <Bwalker8038@gmail.com>
2011-10-05 17:37:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Porter::Application.config.secret_token = 'ef4e2646db78b84f85ec0ea80f6413284479219b1b4f03eae1465bae0088fcd9d95f18ba11087bda79ba170308d2bb6cab1b89ac3a02222b2f82391b807724c2'
