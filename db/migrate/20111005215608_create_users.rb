84891897b1277ce0b831fcbc031e77492e71dd63
Brad Walker <Bwalker8038@gmail.com>
2011-10-05 17:57:21 -0400

    generated users model

---
+++ db/migrate/20111005215608_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :realname
+      t.string :email
+      t.string :password_hash
+      t.string :password_salt
+
+      t.timestamps
+    end
+  end
+end
